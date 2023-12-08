ac542fc8ce675fab6f29b0979b9179a554af6671
Dachau <1365368391@qq.com>
2023-12-08 15:33:54 +0800

    fix the missing values

--- Classification_Titanic/README.md
+++ Classification_Titanic/README.md
@@ -13,11 +13,26 @@
 
 数据文件为`csv`格式的文本文件，可以使用 `pandas`库读取，具体的数据格式如下图所列：
 
-![data description1](images/data_description1.png)
+<img src="images/data_description.png" alt="data description" style="zoom:67%;" />
 
-train数据共有11列
+train数据共有12列
 
-test数据共有10列， 比train少了survival列
+test数据共有11列， 比train少了survival列
+
+每列含义：
+
+1. **PassengerId（乘客ID）:** 乘客在数据集中的唯一标识。
+2. **Survived（是否幸存）:** 目标变量，表示乘客是否在泰坦尼克号沉没事件中幸存。1表示幸存，0表示未幸存。
+3. **Pclass（舱位等级）:** 乘客所在的舱位等级，有三个值：1st（上层）、2nd（中层）、3rd（下层）。
+4. **Name（姓名）:** 乘客的姓名。
+5. **Sex（性别）:** 乘客的性别，值为 "male"（男性）或 "female"（女性）。
+6. **Age（年龄）:** 乘客的年龄。可能包含缺失值。
+7. **SibSp（同辈亲属数量）:** 乘客在船上有多少兄弟姐妹或配偶。
+8. **Parch（非同辈亲属数量）:** 乘客在船上有多少父母或子女。
+9. **Ticket（船票号码）:** 乘客的船票号码。
+10. **Fare（票价）:** 乘客支付的船票费用。
+11. **Cabin（客舱号码）:** 乘客的客舱号码。可能包含缺失值。
+12. **Embarked（登船港口）:** 乘客登船的港口，有三个值：C（瑟堡）、Q（皇后镇）、S（南安普顿）。可能包含缺失值。
 
 
 
@@ -36,6 +51,23 @@ print(train_data.info())
 print('-' * 30)
 print(test_data.info())
 ```
+
+<img src="images/data_missing.png" alt="data_missing" style="zoom:50%;" />
+
+可见train数据集中Age列和Cabin列大量缺失，Embarked少量缺失
+
+test数据集中Cabin列大量缺失，Fare列少量缺失
+
+**缺失值的处理：**
+
+- 对于 `Age` 列：由于缺失值较多，因此不能直接删除缺失列，使用其他特征预测年龄。
+- 对于 `Cabin` 列：由于缺失值较多，而且该特征较分散、随机。可以考虑将缺失和非缺失分为两个类别，或者在分析中不使用这一列。我选择分为缺失、非缺失两个类别。
+- 对于 `Embarked` 列：由于缺失值较少，考虑使用众数填充。
+- 对于 `Fare` 列：可以使用均值或中位数填充缺失值。
+
+处理缺失值之后：
+
+<img src="images/data_fixed.png" alt="data_fixed" style="zoom:50%;" />
 
 
 
--- Classification_Titanic/data_processing.py
+++ Classification_Titanic/data_processing.py
@@ -1,9 +1,56 @@
+import pandas as pd
+
 # 数据列名：PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
 # 读取数据
 train_data = pd.read_csv('./data/train.csv', encoding='utf-8')
 test_data = pd.read_csv('./data/test.csv', encoding='utf-8')
 
 # 查看是否有缺失值
+print(train_data.info())
+print('-' * 30)
+print(test_data.info())
+
+# 预测 Age 列的缺失值
+def age_predict(df):
+    # 将 Age 不缺失的项作为训练集，Age 缺失的项作为测试集
+    age_df = df[['Age', 'Fare', 'Parch', 'SibSp', 'Pclass']]
+    age_df_notnull = age_df.loc[(df['Age'].notnull())]
+    age_df_isnull = age_df.loc[(df['Age'].isnull())]
+    # print(age_df_notnull)
+    # print(age_df_isnull)
+
+    # 将训练集和测试集分为特征和标签
+    X = age_df_notnull.values[:, 1:]
+    y = age_df_notnull.values[:, 0]
+
+    # 使用随机森林回归模型进行训练
+    from sklearn.ensemble import RandomForestRegressor
+    rfr = RandomForestRegressor(n_estimators=1000, n_jobs=-1)
+    rfr.fit(X, y)
+
+    # 使用训练好的模型进行预测
+    predictedAges = rfr.predict(age_df_isnull.values[:, 1:])
+    df.loc[(df['Age'].isnull()), 'Age'] = predictedAges
+
+    return df
+
+# 处理 Cabin 列的缺失值，将缺失和非缺失分为两个类别
+train_data['Cabin'] = train_data['Cabin'].apply(lambda x: 0 if pd.isnull(x) else 1)
+test_data['Cabin'] = test_data['Cabin'].apply(lambda x: 0 if pd.isnull(x) else 1)
+
+# 处理 Embarked 列的缺失值，用众数填充
+train_data['Embarked'].fillna(train_data['Embarked'].mode()[0], inplace=True)
+
+# 处理 Fare 列的缺失值，用均值填充
+test_data['Fare'].fillna(test_data['Fare'].mean(), inplace=True)
+
+# 处理 Age 列的缺失值，使用其他列的数据预测 Age 列的缺失值
+# train_data['Age'].fillna(train_data['Age'].median(), inplace=True)
+# test_data['Age'].fillna(test_data['Age'].median(), inplace=True)
+train_data = age_predict(train_data)
+test_data = age_predict(test_data)
+
+# 处理缺失值后再查看是否有缺失值
 print(train_data.info())
 print('-' * 30)
 print(test_data.info())
